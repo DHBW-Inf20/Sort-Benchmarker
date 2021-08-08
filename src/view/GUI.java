869022cf0e8166de62f1a370fcf5f02f29ddbc0c
Fabi <fabian.klimpel2002@gmail.com>
2021-08-08 15:00:02 +0200

    added scrollpane sidebar

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -3,18 +3,20 @@ package view;
 import view.components.SortSelection;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class GUI extends JFrame {
 
     public GUI() {
-        super("Test GUI");
+        super("Sort-Benchmarker");
         this.initFrame();
         this.setVisible(true);
     }
 
     private void initFrame() {
-        this.setSize(500, 500);
-        this.setResizable(false);
+        this.setSize(800, 800);
+        this.setResizable(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
@@ -23,13 +25,19 @@ public class GUI extends JFrame {
     }
 
     private void initPanel(JPanel panel) {
-        panel.setLayout(null);
+        panel.setLayout(new BorderLayout());
 
-        JLabel test = new JLabel("Testttttttttttttttttttttttttttttttttttttttttttttttttttt");
-        test.setSize(100, 100);
-        test.setLocation(50, 50);
-        test.setVisible(true);
-        panel.add(test);
+//        JLabel test = new JLabel("Testttttttttttttttttttttttttttttttttttttttttttttttttttt");
+//        test.setSize(100, 100);
+//        test.setLocation(50, 50);
+//        panel.add(test);
+
+        SortSelection sortSelection = new SortSelection();
+        sortSelection.setBackground(Color.BLUE);
+
+        // Set width from sort selection panel
+        sortSelection.setPreferredSize(new Dimension(400, 0));
+        panel.add(sortSelection, BorderLayout.WEST);
 
         panel.setVisible(true);
     }
--- src/view/components/SortSelection.java
+++ src/view/components/SortSelection.java
@@ -1,10 +1,27 @@
 package view.components;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class SortSelection extends JPanel {
 
     public SortSelection() {
+        setLayout(new BorderLayout());
 
+        JPanel panel = new JPanel();
+        init(panel);
+        JScrollPane scrollPane = new JScrollPane(panel, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void init(JPanel panel) {
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        for (int i = 0; i < 20; i++) {
+            JLabel test = new JLabel("Testtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt");
+            panel.add(test);
+        }
+
+        panel.setVisible(true);
     }
 }
