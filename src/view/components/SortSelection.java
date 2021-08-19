4ab823a5adc4dc1750b87733f5fc85d4eedd2cc5
Fabi <fabian.klimpel2002@gmail.com>
2021-08-19 15:25:10 +0200

    add AlgorithmComponent Class in SortSelection

--- src/view/components/SortSelection.java
+++ src/view/components/SortSelection.java
@@ -26,8 +26,7 @@ public class SortSelection extends JPanel {
         selectPane.getVerticalScrollBar().setUnitIncrement(8);
 
         JPanel selectedPanel = new JPanel();
-        // TODO: initSelectedPane
-        initSelectPane(selectedPanel);
+        initSelectedPane(selectedPanel);
         JScrollPane selectedPane = new JScrollPane(selectedPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         selectedPane.getVerticalScrollBar().setUnitIncrement(8);
 
@@ -73,34 +72,41 @@ public class SortSelection extends JPanel {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         for (int i = 0; i < 5; i++) {
-            panel.add(getAlgorithmComponent("Algorithmus " + i));
+            panel.add(new AlgorithmComponent("Algorithmus " + i, true));
         }
     }
 
-    private JComponent getAlgorithmComponent(String name) {
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    private void initSelectedPane(JPanel panel) {
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        Font font = new Font("Arial", Font.PLAIN, 20);
+        for (int i = 0; i < 3; i++) {
+            panel.add(new AlgorithmComponent("Algorithmus " + i, false));
+        }
+    }
 
-        JButton btn = new JButton("+");
-        btn.setOpaque(false);
-        btn.setContentAreaFilled(false);
-//        btn.setBorderPainted(false);
-        btn.setFont(font);
+    private static class AlgorithmComponent extends JPanel {
+        public AlgorithmComponent(String name, boolean add_remove) {
+            setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        btn.addActionListener(e -> {
-            System.out.println(name);
-        });
+            Font font = new Font("Arial", Font.PLAIN, 20);
 
-        JLabel label = new JLabel(name);
-        label.setFont(font);
+            JButton btn = new JButton(add_remove ? "+" : "-");
+            btn.setOpaque(false);
+            btn.setContentAreaFilled(false);
+//            btn.setBorderPainted(false);
+            btn.setFont(font);
 
-        panel.add(btn);
-        panel.add(label);
+            btn.addActionListener(e -> {
+                System.out.println(name);
+            });
 
-        panel.setMaximumSize(new Dimension(10000, (int) panel.getPreferredSize().getHeight()));
+            JLabel label = new JLabel(name);
+            label.setFont(font);
 
-        return panel;
+            add(btn);
+            add(label);
+
+            setMaximumSize(new Dimension(10000, (int) getPreferredSize().getHeight()));
+        }
     }
 }
