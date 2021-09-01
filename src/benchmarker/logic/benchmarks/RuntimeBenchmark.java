8bd7fa80793d4cb7737e842a19a7ff5a3dda4f4a
Fabi <fabian.klimpel2002@gmail.com>
2021-09-01 16:45:30 +0200

    improved RuntimeBenchmark

--- src/benchmarker/logic/benchmarks/RuntimeBenchmark.java
+++ src/benchmarker/logic/benchmarks/RuntimeBenchmark.java
@@ -8,17 +8,22 @@ import benchmarker.utils.options.Option;
 import benchmarker.utils.options.OptionType;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class RuntimeBenchmark extends Benchmark {
 
     private final ArrayList<String[]> contents;
+
+    private boolean showBars;
 
     public RuntimeBenchmark() {
         contents = new ArrayList<>();
 
         addOption(new Option("Array-Größe", OptionType.NUMBER, 1000000));
         addOption(new Option("Iterationen", OptionType.NUMBER, 10));
+        addOption(new Option("Balken zeigen", OptionType.BOOL, true));
     }
 
     @Override
@@ -32,6 +37,7 @@ public class RuntimeBenchmark extends Benchmark {
 
         int arraySize = (int) getValue("Array-Größe");
         int iterations = (int) getValue("Iterationen");
+        showBars = (boolean) getValue("Balken zeigen");
 
         Random random = new Random();
         long[] seeds = new long[iterations];
@@ -39,6 +45,8 @@ public class RuntimeBenchmark extends Benchmark {
             seeds[i] = random.nextLong();
         }
 
+        long globalMax = Long.MIN_VALUE;
+        HashMap<Sorter, HashMap<String, Object>> datas = new HashMap<>();
         ArrayList<Long> tempResults = new ArrayList<>();
         for (Sorter sorter : sortPool) {
             if (!sorter.passedTests()) continue;
@@ -68,7 +76,17 @@ public class RuntimeBenchmark extends Benchmark {
 
             contents.add(new String[] {sorter.getName(), numberToString(mean), numberToString(min), numberToString(max)});
 
+            if (max > globalMax) globalMax = max;
+            datas.put(sorter, result);
+
             updateResult(sorter, result);
+        }
+        if (showBars) {
+            for (Sorter sorter : datas.keySet()) {
+                HashMap<String, Object> result = datas.get(sorter);
+                result.put("globalMax", globalMax);
+                updateResult(sorter, result);
+            }
         }
     }
 
@@ -81,6 +99,21 @@ public class RuntimeBenchmark extends Benchmark {
         double mean = (Double) result.get("mean");
         long min = (Long) result.get("min");
         long max = (Long) result.get("max");
+
+        if (showBars) {
+            if (result.get("globalMax") != null) {
+                long globalMax = (Long) result.get("globalMax");
+                System.out.println(globalMax);
+                Bar bar = new Bar(mean / (double)  globalMax, min / (double) globalMax, max / (double) globalMax);
+                bar.setPreferredSize(new Dimension(800, 30));
+                panel.add(bar);
+            } else {
+                Bar bar = new Bar(-1, -1, -1);
+                bar.setPreferredSize(new Dimension(800, 30));
+                panel.add(bar);
+            }
+            panel.add(Box.createVerticalStrut(10));
+        }
 
         JLabel meanDuration = new JLabel("Mittlere Laufzeit: " + Math.round(mean * 100) / 100d + " ms");
         meanDuration.setFont(Settings.font);
@@ -101,6 +134,34 @@ public class RuntimeBenchmark extends Benchmark {
         csvUtils.setHeader(new String[] {"Algorithmus", "Mittlere Laufzeit", "Minimale Laufzeit", "Maximale Laufzeit"});
         for (String[] content : contents) {
             csvUtils.addContent(content);
+        }
+    }
+
+    public static class Bar extends JComponent {
+
+        private final double mean;
+        private final double min;
+        private final double max;
+
+        public Bar(double mean, double min, double max) {
+            this.mean = mean;
+            this.min = min;
+            this.max = max;
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            g.setColor(new Color(192, 192, 192));
+            g.fillRect(0, 5, getWidth(), getHeight() - 10);
+            if (mean != -1) {
+                g.setColor(new Color(204, 0, 0));
+                g.fillRect(0, 5, (int) (getWidth() * max), getHeight() - 10);
+                g.setColor(new Color(255, 173, 51));
+                g.fillRect(0, 5, (int) (getWidth() * min), getHeight() - 10);
+                g.setColor(new Color(0, 160, 0));
+                g.fillRect((int) (getWidth() * mean) - 2, 0, 4, getHeight());
+            }
         }
     }
 }
