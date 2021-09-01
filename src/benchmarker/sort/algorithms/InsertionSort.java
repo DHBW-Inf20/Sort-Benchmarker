39ddf73816fa5c1d5ca9244a32538c50d259c3ae
Luca <i20024@hb.dhbw-stuttgart.de>
2021-09-01 17:27:12 +0200

    Commented normal Sort algorithms

--- src/benchmarker/sort/algorithms/InsertionSort.java
+++ src/benchmarker/sort/algorithms/InsertionSort.java
@@ -15,9 +15,9 @@ public class InsertionSort extends Sorter {
     public int[] sort(int[] toSort) {
         unsortedArray = toSort;
         int temp;
-        for (int i = 0; i < unsortedArray.length; i++) {
-            for (int j = unsortedArray.length-1; j > 0; j--) {
-                if (unsortedArray[j-1] > unsortedArray[j]) {
+        for (int i = 0; i < unsortedArray.length; i++) { //Einmal durch das Array
+            for (int j = unsortedArray.length-1; j > 0; j--) { //Rückwärts durch das Array
+                if (unsortedArray[j-1] > unsortedArray[j]) { //Wenn das  linkere Element kleiner ist als das rechtere wird es getauscht
                     temp = unsortedArray[j];
                     unsortedArray[j] = unsortedArray[j - 1];
                     unsortedArray[j - 1] = temp;
