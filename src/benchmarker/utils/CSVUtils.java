30f341bfbc6cf1ef6017868ca7432c5ff4c27d55
JFreudenberger <i20013@hb.dhbw-stuttgart.de>
2021-09-01 19:29:01 +0200

    Decription of CSVUtils - JF

--- src/benchmarker/utils/CSVUtils.java
+++ src/benchmarker/utils/CSVUtils.java
@@ -4,8 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class CSVUtils {
@@ -15,15 +13,28 @@ public class CSVUtils {
     private final ArrayList<String[]> contents;
     private String[] header;
 
+    /**
+     * Konstruktor zur Initialisierung der CSV-Datei und des Inhalts
+     * @param file      File-Objekt zur Speicherung der Daten in CSV-Format
+     */
     public CSVUtils(File file) {
         this.file = file;
-        contents = new ArrayList<>();
+        contents = new ArrayList<>();   // Leere Arrayliste für die zu speichernden Daten
     }
 
+    /**
+     * Zuweisung der Spaltenüberschrift für die CSV-Datei
+     * @param header        Name der Spaltenüberschrift
+     */
     public void setHeader(String[] header) {
         this.header = header;
     }
 
+    /**
+     *
+     * @param content
+     * @return
+     */
     public boolean addContent(String[] content) {
         if (header.length == content.length) {
             contents.add(content);
@@ -32,6 +43,12 @@ public class CSVUtils {
         return false;
     }
 
+    /**
+     * Versuch die Daten in die CSV-Datei einzufügen.
+     * @return              Wahr, wenn das Schreiben in die CSV-Datei erfolgreich.
+     *                      Falsch, ansonsten.
+     * @throws IOException  Fehlermeldung bei missglücktem Zugriff auf die CSV-Datei.
+     */
     public boolean writeFile() throws IOException {
         if (header == null || contents.isEmpty()) {
             return false;
